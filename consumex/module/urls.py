4d058fc853e09813266af73da26c3252dd054de7
glen <glenpop54@gmail.com>
2018-10-28 11:39:43 +0530

    initial commit

--- consumex/module/urls.py
+++ consumex/module/urls.py
@@ -1,6 +1,10 @@
 from django.urls import re_path
-from .views import HomeView
+from .views import HomeView, ContactView, AboutView, PortFolioView, ServicesView
 
 urlpatterns = [
-    re_path('^$', HomeView.as_view(), name = 'home'),
+    re_path('^$', HomeView.as_view(), name='home'),
+    re_path('^contact-us$', ContactView.as_view(), name='contact-us'),
+    re_path('^about$', AboutView.as_view(), name='about'),
+    re_path('^portfolio$', PortFolioView.as_view(), name='portfolio'),
+    re_path('^services/$', ServicesView.as_view(), name='services'),
 ]
--- consumex/module/views.py
+++ consumex/module/views.py
@@ -1,5 +1,22 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
+
 class HomeView(TemplateView):
     template_name = 'index.html'
+
+
+class ContactView(TemplateView):
+    template_name = 'contact.html'
+
+
+class PortFolioView(TemplateView):
+    template_name = 'portfolio.html'
+
+
+class AboutView(TemplateView):
+    template_name = 'about.html'
+
+
+class ServicesView(TemplateView):
+    template_name = 'services.html'
